92081a154916824bd82c114cb0ff68980b1c94ef
student <ikulovac1@etf.unsa.ba>
2019-05-27 11:17:05 +0200

    priprema

---
+++ src/ba/unsa/rs/tutorijal10/Grad.java
@@ -0,0 +1,38 @@
+package ba.unsa.rs.tutorijal10;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Grad implements Serializable {
+
+    String naziv;
+    int brojStanovnika;
+    double[] temperature = new double[1000];
+
+    public Grad() {
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public double[] getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double[] temperature) {
+        this.temperature = temperature;
+    }
+}
